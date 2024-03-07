616776b718b06bc4004409416e7a097a3e551c99
Julian Monroy <julianmonroy924@gmail.com>
2024-03-07 18:52:54 -0500

    Challenge 1 of 3: Fix the mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import TodoList from "../TodoList"
 import Congratulations from "./Congratulations"
 import Gallery from "./Gallery"
 import {Profile} from "./Profile"
@@ -9,6 +10,7 @@ function App() {
       <Gallery />
       <Profile />
       <Congratulations />
+      <TodoList />
      
     </>
   )

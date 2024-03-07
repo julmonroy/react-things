6cf4ed919d0cbb7f9186ebaec09b024abc7bab55
Julian Monroy <julianmonroy924@gmail.com>
2024-03-07 15:36:07 -0500

    Challenge 2 of 4: Fix the return statement

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,14 +1,9 @@
 function Profile() {
-    return (
-        <>
-      <img
+    return <img
         src="https://i.imgur.com/lICfvbD.jpg"
         alt="Aklilu Lemma"
       /> 
-      <p>Test</p> 
-      <div></div>
-      </>
-    );
+      
   }
   export default Profile
 

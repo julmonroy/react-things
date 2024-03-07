a221f7e2af1548d66b1c8eb53021d490bde95cf9
Julian Monroy <julianmonroy924@gmail.com>
2024-03-07 15:28:57 -0500

    Challenge 1 of 4: Export the component

---
+++ src/Profile.jsx
@@ -0,0 +1,14 @@
+function Profile() {
+    return (
+        <>
+      <img
+        src="https://i.imgur.com/lICfvbD.jpg"
+        alt="Aklilu Lemma"
+      /> 
+      <p>Test</p> 
+      <div></div>
+      </>
+    );
+  }
+  export default Profile
+

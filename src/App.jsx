dec35ecc97649468e2a13f1f7e6e564c26adfd1e
Julian Monroy <julianmonroy924@gmail.com>
2024-03-07 15:46:01 -0500

    4 of 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Congratulations from "./Congratulations"
 import Gallery from "./Gallery"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Gallery />
+      <Congratulations />
     </>
   )
 }

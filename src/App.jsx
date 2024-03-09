8275957a45dd527ef56d0820d33d528bcd7b43bd
Julian Monroy <julianmonroy924@gmail.com>
2024-03-09 00:50:16 -0500

    Challenge 2 of 3: Adjust the image size based on a prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,38 @@
 import { getImageUrl } from './utils.js';
 
-function Profile({ person, imageSize = 70 }) {
-  const imageSrc = getImageUrl(person)
-
+function Avatar({ person, size }) {
+  let thumbnailSize = 's';
+  if (size > 90) {
+    thumbnailSize = 'b';
+  }
   return (
-    <section className="profile">
-      <h2>{person.name}</h2>
-      <img
-        className="avatar"
-        src={imageSrc}
-        alt={person.name}
-        width={imageSize}
-        height={imageSize}
-      />
-      <ul>
-        <li>
-          <b>Profession:</b> {person.profession}
-        </li>
-        <li>
-          <b>Awards: {person.awards.length} </b>
-          ({person.awards.join(', ')})
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {person.discovery}
-        </li>
-      </ul>
-    </section>
-  )
+    <img
+      className="avatar"
+      src={getImageUrl(person, thumbnailSize)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
 }
 
-export default function Gallery() {
+export default function Profile() {
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile person={{ 
-        imageId: 'szV5sdG',
-        name: 'Maria Skłodowska-Curie',
-        profession: 'physicist and chemist',
-        discovery: 'polonium (chemical element)',
-        awards: [
-          'Nobel Prize in Physics',
-          'Nobel Prize in Chemistry',
-          'Davy Medal',
-          'Matteucci Medal'
-        ],
-      }} />
-      <Profile person={{
-        imageId: 'YfeOqp2',
-        name: 'Katsuko Saruhashi',
-        profession: 'geochemist',
-        discovery: 'a method for measuring carbon dioxide in seawater',
-        awards: [
-          'Miyake Prize for geochemistry',
-          'Tanaka Prize'
-        ],
-      }} />
-    </div>
+    <>
+      <Avatar
+        size={40}
+        person={{ 
+          name: 'Gregorio Y. Zara', 
+          imageId: '7vQD0fP'
+        }}
+      />
+      <Avatar
+        size={120}
+        person={{ 
+          name: 'Gregorio Y. Zara', 
+          imageId: '7vQD0fP'
+        }}
+      />
+    </>
   );
 }

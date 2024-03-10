6807a917f1b01533030890ae931496651bba15ed
Julian Monroy <julianmonroy924@gmail.com>
2024-03-10 19:38:46 -0400

    Challenge 2 of 4: Nested lists in one component

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,23 @@
-import { people } from './data.js';
-import { getImageUrl } from './utils.js';
+import { recipes } from './data.js';
 
-export default function List() {
-  const chemisList = people.filter(person => 
-    person.profession === 'chemist');
+export default function RecipeList() {
 
-  const elselist = people.filter(person => 
-    person.profession !== 'chemist');
 
     return (
-    <article>
-      <h1>Chemists</h1>
-      <ul>
-        {chemisList.map(person => 
-          <li key={person.id}>
-            <img
-              src={getImageUrl(person)}
-              alt={person.name}
-            />
-            <p>
-              <b>{person.name}:</b>
-              {' ' + person.profession + ' '}
-              known for {person.accomplishment}
-            </p>
-          </li>
-        )}
-      </ul>
-      <h1>Everyone else</h1>
-      <ul>
-        {elselist.map(person => 
-          <li key={person.id}>
-            <img
-              src={getImageUrl(person)}
-              alt={person.name}
-            />
-            <p>
-              <b>{person.name}:</b>
-              {' ' + person.profession + ' '}
-              known for {person.accomplishment}
-            </p>
-          </li>
-        )}
-      </ul>
-   
-    </article>
-  );
-        }
+    <div>
+      {recipes.map(recipe => 
+        <div key = {recipe.id}>
+      
+    <h2>{recipe.name}</h2>
+
+    <ul>
+      {recipe.ingredients.map(ingridients => 
+        <li key={ingridients}>{ingridients}</li>
+      )}
+    </ul>
+    </div>
+)}
+  </div>
+    );
+}
+  
